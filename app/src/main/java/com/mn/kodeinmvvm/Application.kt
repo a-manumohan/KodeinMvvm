5f3a09ae41418c6dc5ef340ae8fa6bfcae670331
Manu Mohan <manu.mohan@tajawal.com>
2018-04-02 12:34:15 +0400

    Initial Commit

---
+++ app/src/main/java/com/mn/kodeinmvvm/Application.kt
@@ -0,0 +1,10 @@
+package com.mn.kodeinmvvm
+
+import android.app.Application
+import com.github.salomonbrys.kodein.Kodein
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
